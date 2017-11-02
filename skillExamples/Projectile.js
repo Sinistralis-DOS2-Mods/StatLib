55eccb191a3618f58229cbef54b7daa49cb9cfa4
mzimmerman <mzimmerman0205@gmail.com>
2017-11-02 04:07:38 -0400

    Some more documentation

--- skillExamples/Projectile.js
+++ skillExamples/Projectile.js
@@ -1,5 +1,8 @@
-//const WEAPON_MODIFIERS = require('../lib/const/weaponModifiers');
+// A listing of all fields available for Projectile. One of these exist for each respective skill type.
 const { PROJECTILE_NAMES } = require('../lib/definitions/skillFields');
+
+// A list of all enums available within the Editor. If you use the field name helpers listed above and you have VSCode or another editor that supports JSDoc,
+// intellisense you will you the ENUM to import for a given field, if required.
 const { SKILL_REQUIREMENT } = require('../lib/definitions/enums');
 
 const skills = [
